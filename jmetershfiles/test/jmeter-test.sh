340350986d1f01d072b58d6448838dbe09a7c584
João <joao.victor_lopes27@outlook.com>
2021-03-30 11:12:20 -0300

    Testes do MapServer finalizados!

--- jmetershfiles/test/jmeter-test.sh
+++ jmetershfiles/test/jmeter-test.sh
@@ -1,6 +1,6 @@
 # Equinfra
-jmeter -Jthreads=20000 -Jloops=5 --nongui --reportatendofloadtests --testfile jmxfiles/equinfra.jmx --logfile log.txt --reportoutputfolder results/containers3_users20000_loops5
-rm log.txt jmeter.log
+#jmeter -Jthreads=20000 -Jloops=5 --nongui --reportatendofloadtests --testfile jmxfiles/equinfra.jmx --logfile log.txt --reportoutputfolder results/containers3_users20000_loops5
+#rm log.txt jmeter.log
 
 # GeoServer
 #sudo docker container start geoserver
@@ -10,3 +10,7 @@ rm log.txt jmeter.log
 #sleep 5m
 #rm -rf jmeter.log log.txt results
 #sudo docker container stop geoserver
+
+# MapServer
+jmeter -Jthreads=100 -Jloops=1 -Jip=172.17.0.2 -Jport=80 --nongui --reportatendofloadtests --testfile jmxfiles/mapserver.jmx --logfile log.txt --reportoutputfolder results/users100_loops1_rodada1
+rm log.txt jmeter.log
