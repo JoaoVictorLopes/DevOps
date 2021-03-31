5deb70764bbd47d50e31bdca1055a325f1eb5d21
João <joao.victor_lopes27@outlook.com>
2021-03-31 14:42:33 -0300

    Finalizando testes principais

--- jmetershfiles/test/jmeter-test.sh
+++ jmetershfiles/test/jmeter-test.sh
@@ -12,5 +12,6 @@
 #sudo docker container stop geoserver
 
 # MapServer
-jmeter -Jthreads=100 -Jloops=1 -Jip=172.17.0.2 -Jport=80 --nongui --reportatendofloadtests --testfile jmxfiles/mapserver-municipios-geojson.jmx --logfile log.txt --reportoutputfolder results/users100_loops1_rodada1
+jmeter -Jthreads=200 -Jloops=1 -Jip=172.17.0.2 -Jport=8080 --nongui --reportatendofloadtests --testfile jmxfiles/geoserver-rodovias-geojson.jmx --logfile log.txt --reportoutputfolder results/users200_loops1_rodada1
 rm log.txt jmeter.log
+sudo mv results/users200_loops1_rodada1 /var/www/html/tests-geo-servers/geoserver/consulta2/
