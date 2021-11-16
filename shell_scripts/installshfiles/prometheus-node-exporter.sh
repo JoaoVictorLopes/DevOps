1f4ddb011b148443781ca3ff3b253f79fe958684
João Victor Lopes <joao.victor_lopes27@outlook.com>
2021-11-16 21:58:03 +0000

    prometheus configuration v3

---
+++ shell_scripts/installshfiles/prometheus-node-exporter.sh
@@ -0,0 +1,4 @@
+wget https://github.com/prometheus/node_exporter/releases/download/v1.2.2/node_exporter-1.2.2.linux-amd64.tar.gz
+tar --extract --file node_exporter-1.2.2.linux-amd64.tar.gz
+cd node_exporter-1.2.2.linux-amd64
+./node_exporter
--- shell_scripts/installshfiles/prometheus-server.sh
+++ shell_scripts/installshfiles/prometheus-server.sh
@@ -1,4 +1,5 @@
 wget https://github.com/prometheus/prometheus/releases/download/v2.31.1/prometheus-2.31.1.linux-amd64.tar.gz
 tar --extract --file prometheus-2.31.1.linux-amd64.tar.gz
-cd prometehus-2.31.1.linux-amd64
+echo '  - job_name: "node"\n    static_configs:\n      - targets: ["localhost:9100"]\n' >> prometheus-2.31.1.linux-amd64/prometheus.yml
+cd prometheus-2.31.1.linux-amd64
 ./prometheus
