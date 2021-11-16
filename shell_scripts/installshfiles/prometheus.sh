a94903f5eff1845c53836cc324f6d1c4c62c2204
João Victor Lopes <joao.victor_lopes27@outlook.com>
2021-11-16 20:47:58 +0000

    prometheus configuration v2

---
+++ shell_scripts/installshfiles/prometheus.sh
@@ -0,0 +1,4 @@
+wget https://github.com/prometheus/prometheus/releases/download/v2.31.1/prometheus-2.31.1.linux-amd64.tar.gz
+tar --extract --file prometheus-2.31.1.linux-amd64.tar.gz
+cd prometehus-2.31.1.linux-amd64
+./prometheus
