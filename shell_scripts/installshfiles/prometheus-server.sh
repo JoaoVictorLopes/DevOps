cb7dbcf9bd1e394ea1bb8ed93f0eb6099b253e59
João Victor Lopes <joao.victor_lopes27@outlook.com>
2021-11-21 22:58:50 +0000

    simulation web application v3

--- shell_scripts/installshfiles/prometheus-server.sh
+++ shell_scripts/installshfiles/prometheus-server.sh
@@ -4,5 +4,6 @@ echo '{\n  "metrics-addr" : "127.0.0.1:9323",\n  "experimental" : true\n}' > /et
 echo '  - job_name: "docker"\n    static_configs:\n      - targets: ["localhost:9323"]\n' >> prometheus-2.31.1.linux-amd64/prometheus.yml
 echo '  - job_name: "node"\n    static_configs:\n      - targets: ["localhost:9100"]\n' >> prometheus-2.31.1.linux-amd64/prometheus.yml
 echo '  - job_name: "cadvisor"\n    scrape_interval: 5s\n    static_configs:\n      - targets:\n        - cadvisor:8080' >> prometheus-2.31.1.linux-amd64/prometheus.yml
+echo '  - job_name: "spring"\n    static_configs:\n      - targets: ["localhost:1234"]\n' >> prometheus-2.31.1.linux-amd64/prometheus.ym
 cd prometheus-2.31.1.linux-amd64
 ./prometheus
