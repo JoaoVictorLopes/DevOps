7f2b3126aba0f06e66d912d3552f4e1e365762b8
João Victor Lopes <joao.victor_lopes27@outlook.com>
2021-11-23 01:52:07 +0000

    update

--- sgeol-app/src/main/java/sgeol/App.java
+++ sgeol-app/src/main/java/sgeol/App.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import io.prometheus.client.Counter;
 import io.prometheus.client.exporter.HTTPServer;
+import io.prometheus.client.spring.web.EnablePrometheusTiming;
+import io.prometheus.client.spring.web.PrometheusTimeMethod;
 
 /**
  * Hello world!
@@ -15,16 +17,16 @@ import io.prometheus.client.exporter.HTTPServer;
  */
 @SpringBootApplication
 @RestController
+@EnablePrometheusTiming
 public class App
 {
-    static final Counter requests = Counter.build().name("sgeol_requests_total").help("Sgeol total requests.").register();
+    static final Counter counter = Counter.build().name("sgeol_http_requests_total").help("Total de solicitações HTTP do SGeoL.").register();
 
     public static void main( String[] args )
     {
         SpringApplication.run(App.class, args);
-        System.out.println( "Hello World!" );
         try {
-          HTTPServer server = new HTTPServer(1234);
+          HTTPServer server = new HTTPServer(9200);
         }
         catch (Exception e) {
           System.out.println("Falhou!");
@@ -32,8 +34,9 @@ public class App
     }
 
     @GetMapping("/")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-        requests.inc();
+    @PrometheusTimeMethod(name = "sgeol_http_request_seconds", help = "Duração em segundos da solicitação HTTP do SGeoL.")
+    public String index(@RequestParam(value = "name", defaultValue = "World") String name) {
+        counter.inc();
         return String.format("Hello %s!", name);
     }
 }
