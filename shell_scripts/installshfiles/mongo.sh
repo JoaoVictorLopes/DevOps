a127ed8f97e021dfd4fe87d7ab72881f966df5e0
João <joao.victor_lopes27@outlook.com>
2021-12-31 20:17:13 -0300

    MongoDB configuration

--- shell_scripts/installshfiles/mongo.sh
+++ shell_scripts/installshfiles/mongo.sh
@@ -1,13 +1,14 @@
 # INSTALL
 sudo apt update
 sudo apt install gnupg
-wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add -
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/5.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list
+wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+echo "deb [arch=amd64] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
 sudo apt update
 sudo apt install mongodb-org --yes
 
 # REMOVE
 #sudo service mongod stop
-#sudo apt-get purge mongodb-org*
+#sudo apt  purge mongodb-org*
+#sudo apt autoremove
 #sudo rm -r /var/log/mongodb
 #sudo rm -r /var/lib/mongodb
