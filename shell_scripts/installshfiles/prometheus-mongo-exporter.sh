ced6985c6f233f8437120686297ed1f5ca0b9cc5
João <joao.victor_lopes27@outlook.com>
2022-01-13 14:51:08 -0300

    MongoDB Exporter

---
+++ shell_scripts/installshfiles/prometheus-mongo-exporter.sh
@@ -0,0 +1,3 @@
+wget https://github.com/dcu/mongodb_exporter/releases/download/v1.0.0/mongodb_exporter-linux-amd64
+chmod +x mongodb_exporter-linux-amd64
+./mongodb_exporter-linux-amd64 -mongodb.uri 10.7.52.13:27017
