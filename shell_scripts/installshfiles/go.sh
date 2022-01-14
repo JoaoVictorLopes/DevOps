bef5f50668a316ec5d5b424454718c44b1afeb71
João <joao.victor_lopes27@outlook.com>
2022-01-14 16:25:06 -0300

    PostgreSQL Exporter

---
+++ shell_scripts/installshfiles/go.sh
@@ -0,0 +1,4 @@
+wget https://golang.org/dl/go1.16.4.linux-amd64.tar.gz
+tar --extract --file go1.16.4.linux-amd64.tar.gz
+export PATH=$PATH:/home/victor/devops/go/bin
+go version
---
+++ shell_scripts/installshfiles/prometheus-postgres-exporter.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/prometheus-community/postgres_exporter.git
+#cd postgres_exporter
+#make build
+#./postgres_exporter
