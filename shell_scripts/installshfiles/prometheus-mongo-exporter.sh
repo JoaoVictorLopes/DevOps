2d15e34e0dee88a2d20323e400c8435432bf40ae
João <joao.victor_lopes27@outlook.com>
2022-01-14 22:44:38 -0300

    MongoDB Exporter - DCU Failed

--- shell_scripts/installshfiles/prometheus-mongo-exporter.sh
+++ shell_scripts/installshfiles/prometheus-mongo-exporter.sh
@@ -1,3 +1,4 @@
+# MongoDB DCU Failed.
 wget https://github.com/dcu/mongodb_exporter/releases/download/v1.0.0/mongodb_exporter-linux-amd64
 chmod +x mongodb_exporter-linux-amd64
-./mongodb_exporter-linux-amd64 -mongodb.uri 10.7.52.13:27017
+./mongodb_exporter-linux-amd64 -mongodb.uri=localhost:27017
